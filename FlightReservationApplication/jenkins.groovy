b329fe0cb13efd25e08c5d303de11d55a381b0f7
mayurwagh <mayurwagh1412@gmail.com>
2025-07-07 16:13:12 +0530

    pull stage added

--- FlightReservationApplication/jenkins.groovy
+++ FlightReservationApplication/jenkins.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('PULL') {
             steps{
-                
+                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
          stage('Build') {
