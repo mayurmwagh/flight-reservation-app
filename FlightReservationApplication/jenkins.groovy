598f0437a4ebf5d637612d0075c171d556f8914e
mayurwagh <mayurwagh1412@gmail.com>
2025-07-07 18:21:19 +0530

    new pipeline is added

--- FlightReservationApplication/jenkins.groovy
+++ FlightReservationApplication/jenkins.groovy
@@ -1,55 +1,59 @@
 pipeline {
     agent any
+
     stages {
         stage('PULL') {
-            steps{
+            steps {
                 git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
-         stage('Build') {
-            steps{
+
+        stage('Build') {
+            steps {
                 sh '''
                     cd FlightReservationApplication
                     mvn clean package
                 '''
             }
         }
-         stage('Qa-Test') {
-            steps{
+
+        stage('Qa-Test') {
+            steps {
                 withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
-                sh '''
-                    cd FlightReservationApplication
-                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation-backend 
-                '''
+                    sh '''
+                        cd FlightReservationApplication
+                        mvn sonar:sonar -Dsonar.projectKey=flight-reservation-backend 
+                    '''
                 }
             }
         }
+
         stage('Quality-Gate') {
-            steps{
-                timeout(10) {
-                    waitForQualityGate abortPipeline true 
+            steps {
+                timeout(time: 10, unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
                 }
-                
             }
         }
 
         stage('Docker-build') {
-            steps{
+            steps {
                 sh '''
                     cd FlightReservationApplication
-                    docker build -t mayurmwagh/flight-reservation:latest
+                    docker build -t mayurmwagh/flight-reservation:latest .
                     docker push mayurmwagh/flight-reservation:latest
                     docker rmi `docker image list -aq`
-                '''    
+                '''
             }
         }
-        stage('Deploy')
-            steps{
+
+        stage('Deploy') {
+            steps {
                 sh '''
                     cd FlightReservationApplication
                     kubectl apply -f k8s/
                 '''
             }
-
+        }
     }
 }
