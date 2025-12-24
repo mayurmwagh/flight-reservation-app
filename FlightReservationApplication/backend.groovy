9752f352ed690e822ea3206952e7211dbd952ba2
mayurwagh <mayurwagh1412@gmail.com>
2025-12-24 21:28:47 +0530

    configurations added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -25,5 +25,23 @@ pipeline{
             }
             
         }
+        stage('Docker-build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build . -t mayurwagh/flightreservation-new:latest
+                    docker push mayurwagh/flightreservation-new:latest
+                    docker rmi 'docker image list -aq'
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    kubectl apply -f k8s/
+                '''
+            }
+        }
     }
 }
