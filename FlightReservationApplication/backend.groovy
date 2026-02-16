4bfc8d6418a4cde45e31e0a84c805a58627e626c
mayurwagh <mayurwagh1412@gmail.com>
2026-02-16 20:30:02 +0530

    all stpes configured

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,5 +6,42 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
+        stage('Build'){
+            steps{
+                sh '''
+                    cd FlightRerservationApplication
+                    mvn clean package
+                '''
+            }
+        }
+        stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                  sh'''
+                     cd FlightRerservationApplication
+                     mvn soanr:sonar -Dsonar.projectKey=flight-reservation   
+                  '''
+                }
+            }
+        }
+        stage('Docker-build'){
+            steps{
+                sh'''
+                    cd FlightRerservationApplication    
+                    docker build -t mayurwagh/flight-reservation-15-16:latest . 
+                    docker push mayurwagh/flight-reservation-15-16:latest
+                    docker rmi mayurwagh/flight-reservation-15-16:latest
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                sh'''
+                    cd FlightRerservationApplication    
+                    kubectl apply -f k8s/
+                '''
+            }
+        }
+
     }
 }
